f95825263d8c473380c009619736b8ce334818e6
Luis Postigo <69942863+LuisPostigo@users.noreply.github.com>
2025-03-16 14:44:30 -0500

    Update __init__.py

--- pre_processing/__init__.py
+++ pre_processing/__init__.py
@@ -1,4 +1,3 @@
 from .read_and_clean import process_files
-from .applicants2KIF import process_files_in_directory
 
-__all__ = ['process_files', 'process_files_in_directory']
+__all__ = ['process_files']
